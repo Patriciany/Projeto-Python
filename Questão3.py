e53b47a47ffc44f1ef8b7e88f459766d5bf9a277
Patricianny <45316592+Patriciany@users.noreply.github.com>
2018-11-27 17:18:56 -0300

    Create Questão3.py

---
+++ Questão3.py
@@ -0,0 +1,17 @@
+perg = ["Telefonou para a vítima?","Esteve no local do crime?"
+,"Mora perto da vítima?", "Devia para a vítima?",
+"Já trabalhou com a vítima?"]
+respostas = 0 
+si = 0
+for c in perg:
+  respostas = str(input(f"{c} [Sim/Não]: "))
+  if (respostas in "sim"):
+    si += 1
+if (si == 2):
+  print("Suspeito")
+elif( 3 <= si <= 4):
+  print("Cúmplice")
+elif( si == 5):
+  print("Assasino")
+else:
+  print("Inocente")
