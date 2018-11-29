36b0995f74b8145e2e9b34f4cd58ad58d738323b
Patricianny <45316592+Patriciany@users.noreply.github.com>
2018-11-29 10:24:55 -0300

    Update Questão4.py

--- Questão4.py
+++ Questão4.py
@@ -1,23 +1,23 @@
-tipo = str(input("Qual tipo de carne vc vai levar?"))
+tipo = str(input("Qual tipo de carne vc vai levar?")).strip().upper()
 quantidade = int(input("Qual vai ser a Quantidade?"))
-if (tipo == "File Duplo"):
+if (tipo == "FILE DUPLO"):
 	if (quantidade <= 5 ):
 		total = 4.90
 	elif (quantidade > 5):
 		total = 5.80
-elif (tipo == "Alcatra"):
+elif (tipo == "ALCATRA"):
 	if (quantidade <= 5):
 		total = 5.90
 	elif (quantidade > 5):
 		total = 6.80
-elif (tipo == "Picanha"):
+elif (tipo == "PICANHA"):
 	if (quantidade <= 5):
 		total = 6.90
 	elif (quantidade > 5):
 		total = 7.80
 total1 = total * quantidade
-FormaP = str(input("Qual a Forma de Pagamento?"))
-if (FormaP == "Cartão Tabajara"):
+FormaP = str(input("Qual a Forma de Pagamento?")).strip().lower()
+if (FormaP  in 'cartão tabajara cartao tabajara cartaotabajara cartãotabajara'):
 	desconto = (total1 * 5 / 100)
 	total2 = total1 - (total1 * 5 / 100)
 else:
@@ -29,4 +29,4 @@ print("{}Kg De {}".format(quantidade, tipo))
 print("Valor total a Pagar R${:.2f}".format(total1))
 print("Forma de Pagamento: {} ".format(FormaP))
 print("Valor do Desconto: R${:.2f}".format(desconto))
-print ( "Valor total a caso Desconto: R $ {:. 2f}" . format ( total2 ))
+print("Valor Total a Pagar caso haja Desconto: R${:.2f}".format(total2))
